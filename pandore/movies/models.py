e5a92f28cd5ba6eb79b19e618c515da0c3521dd9
Romain Pomier <romain.pomier@gmail.com>
2012-11-16 13:17:29 +0100

    Create models for People and Movies

--- pandore/movies/models.py
+++ pandore/movies/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from people.models import Person
 
-# Create your models here.
+class Genre(models.Model):
+    name = models.CharField(max_length=128)
+
+class Movie(models.Model):
+    title = models.CharField(max_length=256, verbose_name='original title')
+    title_fr = models.CharField(max_length=256, verbose_name='french title')
+    year = models.IntegerField()
+    runtime = models.IntegerField()
+    id_imdb = models.CharField(max_length=7, verbose_name='imdb id')
+    poster = models.CharField(max_length=256, verbose_name='poster url')
+    rating = models.FloatField()
+    nb_rates = models.IntegerField(verbose_name='number of rates')
+    plot = models.TextField()
+    genres = models.ManyToManyField(Genre)
+    persons = models.ManyToManyField(Person, verbose_name='list of people involved', through='Contributors')
+
+class Contributors(models.Model):
+    person = models.ForeignKey(Person)
+    movie = models.ForeignKey(Movie)
+    function = models.CharField(max_length=1)
+
+class Directory(models.Model):
+    movie = models.ForeignKey(Movie)
+    location = models.CharField(max_length=256)
+    quality = models.CharField(max_length=5)
+    size = models.IntegerField(verbose_name='size in MB')
