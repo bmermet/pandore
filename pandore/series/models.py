0c640c5e3990e2761c5c4d7a2f127e1f1ff388f1
Romain Pomier <romain.pomier@gmail.com>
2012-11-16 19:13:39 +0100

    Coding style applyed for series/model.py

--- pandore/series/models.py
+++ pandore/series/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 from people.models import Person
 
+
 class Genre(models.Model):
     name = models.CharField(max_length=128)
+
 
 class Series(models.Model):
     title = models.CharField(max_length=256, verbose_name='original title')
@@ -14,7 +16,9 @@ class Series(models.Model):
     plot = models.TextField()
     language = models.CharField(max_length=2, verbose_name='main language')
     genres = models.ManyToManyField(Genre)
-    persons = models.ManyToManyField(Person, verbose_name='list of people involved', through='SeriesContributors')
+    persons = models.ManyToManyField(
+        Person, verbose_name='list of people involved',
+        through='SeriesContributors')
     number_of_episodes = models.IntegerField(verbose_name='number of episodes')
     number_of_seasons = models.IntegerField(verbose_name='number of seasons')
     begin_year = models.IntegerField(verbose_name='begin year')
@@ -50,7 +54,9 @@ class Episode(models.Model):
     plot = models.TextField()
     date = models.DateField()
     runtime = models.IntegerField()
-    persons = models.ManyToManyField(Person, verbose_name='list of people involved', through='EpisodeContributors')
+    persons = models.ManyToManyField(
+        Person, verbose_name='list of people involved',
+        through='EpisodeContributors')
 
 
 class SeriesContributors(models.Model):
@@ -58,24 +64,29 @@ class SeriesContributors(models.Model):
     series = models.ForeignKey(Series)
     function = models.CharField(max_length=1)
 
+
 class EpisodeContributors(models.Model):
     person = models.ForeignKey(Person)
     episode = models.ForeignKey(Episode)
     function = models.CharField(max_length=1)
 
+
 class SeriesDirectory(models.Model):
     series = models.ForeignKey(Series)
     location = models.CharField(max_length=256)
+
 
 class SeasonDirectory(models.Model):
     season = models.ForeignKey(Season)
     location = models.CharField(max_length=256)
     quality = models.CharField(max_length=5)
 
+
 class EpisodeDirectory(models.Model):
     episode = models.ForeignKey(Episode)
     location = models.CharField(max_length=256)
     quality = models.CharField(max_length=5)
     size = models.IntegerField(verbose_name='size in MB')
-    addition_date = models.DateTimeField(auto_now_add=True, verbose_name='addition date')
+    addition_date = models.DateTimeField(
+        auto_now_add=True, verbose_name='addition date')
 
