0b964d52eb8912355be6b37cbf501e59f3fa45d1
Bertrand Mermet <bertrandmermet@gmail.com>
2012-11-16 20:11:23 +0100

    Added some __unicode__ methods is person and movies models

--- pandore/people/models.py
+++ pandore/people/models.py
@@ -1,4 +1,9 @@
 from django.db import models
 
+
 class Person(models.Model):
+    id_imdb = models.CharField(max_length=7, verbose_name='imdb id')
     name = models.CharField(max_length=128)
+
+    def __unicode__(self):
+        return 'id_imdb: ' + self.id_imdb + '; name: ' + self.name
