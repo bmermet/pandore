22cd9a877c1bc10b8e7ad88c9dda28e46bdddf26
Romain Pomier <romain.pomier@gmail.com>
2012-11-21 21:52:51 +0100

    Adding fields to advanced movie search

--- pandore/movies/advanced_movie_search.py
+++ pandore/movies/advanced_movie_search.py
@@ -5,6 +5,10 @@ from haystack.forms import SearchForm
 class AdvancedMovieSearchForm(SearchForm):
     begin_year = forms.IntegerField(required=False)
     end_year = forms.IntegerField(required=False)
+    min_rating = forms.IntegerField(required=False)
+    max_rating = forms.IntegerField(required=False)
+    min_runtime = forms.IntegerField(required=False)
+    max_runtime = forms.IntegerField(required=False)
 
     def search(self):
         # First, store the SearchQuerySet received from other processing.
@@ -14,10 +18,21 @@ class AdvancedMovieSearchForm(SearchForm):
             return sqs
 
         if self.cleaned_data['begin_year']:
-            print self.cleaned_data['begin_year']
             sqs = sqs.filter(year__gte=self.cleaned_data['begin_year'])
 
         if self.cleaned_data['end_year']:
             sqs = sqs.filter(year__lte=self.cleaned_data['end_year'])
+
+        if self.cleaned_data['min_rating']:
+            sqs = sqs.filter(rating__gte=self.cleaned_data['min_rating'])
+
+        if self.cleaned_data['max_rating']:
+            sqs = sqs.filter(rating__lte=self.cleaned_data['max_rating'])
+
+        if self.cleaned_data['min_runtime']:
+            sqs = sqs.filter(runtime__gte=self.cleaned_data['min_runtime'])
+
+        if self.cleaned_data['max_runtime']:
+            sqs = sqs.filter(runtime__lte=self.cleaned_data['max_runtime'])
 
         return sqs
--- pandore/movies/search_indexes.py
+++ pandore/movies/search_indexes.py
@@ -6,6 +6,14 @@ class MovieIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True, use_template=True)
     title = indexes.EdgeNgramField(model_attr='title')
     year = indexes.IntegerField(model_attr='year')
+    runtime = indexes.IntegerField(model_attr='runtime')
+    rating = indexes.FloatField(model_attr='rating')
+    suggestions = indexes.FacetCharField()
+
+    def prepare(self, obj):
+        prepared_data = super(MovieIndex, self).prepare(obj)
+        prepared_data['suggestions'] = prepared_data['text']
+        return prepared_data
 
     def get_model(self):
         return Movie
