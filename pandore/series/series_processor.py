8ff740d0dbd5d660d3a3abbf88cfb413fc418bd5
Romain Pomier <romain.pomier@gmail.com>
2013-02-10 01:17:16 +0100

    Create SeriesDirectoryProcessor, EpisodeDirectoryProcessor

--- pandore/series/series_processor.py
+++ pandore/series/series_processor.py
@@ -11,6 +11,141 @@ from people.models import Person
 from movies.utils import get_size
 
 
+class SeriesDirectoryProcessor(object):
+    __reg_dir_s = r'(.*/)?(.+)'
+    __reg_bing_s = r'www.imdb.com/title/tt(?P<id>\d+)/'
+    __reg_episode_s = r'(S[0-9]{1,2}E[0-9]{1,3}|\.E[0-9]{1,2}\.|[0-9]{1,2}x[0-9]{1,3})'
+    title = quality = year = id_imdb = None
+
+    def __init__(self):
+        self.__reg_dir = re.compile(self.__reg_dir_s)
+        self.__reg_bing = re.compile(self.__reg_bing_s)
+        self.__reg_episode = re.compile(self.__reg_episode_s)
+        self.reset_infos()
+        self.series_processor = SeriesProcessor()
+        self.episode_dir_processor = EpisodeProcessor()
+        self.ia = IMDb()
+        self.series_on_imdb = None
+
+    def reset_infos(self):
+        self.title = self.quality = self.year = self.id_imdb = None
+        self.directory = None
+        self.processed = False
+        self.series_on_imdb = None
+
+    def process(self, directory):
+        self.reset_infos()
+        self.directory = os.path.abspath(directory)
+
+        dir = self.__reg_dir.match(self.directory).group(2) + '/'
+        guess = guessit.guess_movie_info(dir)
+        if not 'title' in guess:
+            print 'Error while processing ' + self.directory
+            return False
+
+        filter = SeriesDirectory.objects.filter(location=self.directory)
+        if filter.exists():
+            self.series = filter[0]
+        else:
+            # Search series on bing
+            self.year = str(guess['year']) if 'year' in guess else ''
+            search_string = 'site:imdb.com %s %s' % (guess['title'], year)
+            r = requests.get(
+                    'http://www.bing.com/search', params={'q': search_string})
+            match = self.__reg_bing.search(r.content)
+            if not match:
+                print r.url
+                print "Bing wasn't able to find this series, skipping"
+                return False
+            self.id_imdb = match.group('id')
+            self.title = guess['title']
+            self.save()
+
+        # Search episodes in series directory
+        dirs = [self.directory]
+        for d in dirs:
+            if not self.__reg_episode.search(d):
+                for d2 in [d+dir+'/' for dir in os.listdir(d)
+                           if os.path.isdir(d+dir)]:
+                    dirs.append(d2)
+            else:
+                if not self.series_on_imdb:
+                    self.series_on_imdb = self.ia.get_movie(self.series.id_imdb)
+                    self.ia.update(self.series_on_imdb, 'episodes')
+                self.episode_dir_processor.process(d, self.series,
+                        self.series_on_imdb['episodes'])
+
+        return True
+
+    def save(self):
+        self.series = self.series_processor.process(self.id_imdb)
+        SeriesDirectory.objects.create(
+            series=self.series, location=self.directory)
+
+
+class EpisodeDirectoryProcessor()
+    title = quality = year = id_imdb = None
+
+    def __init__(self):
+        self.reset_infos()
+
+    def reset_infos(self):
+        self.title = self.quality = self.year = self.id_imdb = None
+        self.directory = None
+        self.processed = False
+        self.series_on_imdb = None
+        self.episode_processor = EpisodeProcessor()
+
+    def process(self, directory, series_bdd, series_imdb):
+        self.reset_infos()
+        self.path = os.path.abspath(directory)
+        self.filename = os.path.basename(self.path)
+        self.season_directory = os.path.dirname(self.path)
+
+        guess = guessit.guess_movie_info(self.filename)
+        if 'season' in guess.keys and 'episodeNumber' in guess.keys:
+            self.season_nb = guess['season']
+            self.episode_nb = guess.['episodeNumber']
+        else:
+            print self.path
+            print 'Error: Episode wrongly named'
+            return
+        :
+        self.quality = guess['screenSize'] if 'screenSize' in guess else 'SD'
+
+        filter = Season.objects.filter(series=series_bdd,
+                                       season_number=self.season_nb)
+        if filter.exists():
+            self.season = filter[0]
+        else:
+            self.season = Season.objects.create(series=series_bdd,
+                                                season_number=self.season_nb,
+                                                number_of_episodes=0)
+            
+        filter = SeasonDirectory.objects.filter(season=self.season,
+                                                location=self.season_directory,
+                                                quality=self.quality)
+        if filter.exists():
+            self.season_dir = filter[0]
+        else:
+            self.season_dir = SeasonDirectory.objects.create(
+                    season=self.season, location=self.season_directory,
+                    quality=self.quality)
+
+        filter = Episode.objects.filter(season=self.season,
+                                        episode_number=self.episode_nb)
+        if filter.exists():
+            self.episode = filter[0]
+        else:
+            if self.season_nb in series_imdb.keys() and
+                    self.episode_nb in series_imdb[self.season_nb].keys():
+                self.episode = self.episode_processor.process(
+                        series_imdb[self.season_nb][self.episode_nb])
+
+        #TODO: Create Episode Directory
+        #filter = EpisodeDirectory.objects.filter(
+
+
 class SeriesProcessor(object):
     __reg_title_vf_s = r'(.+)::.*France'
     __reg_title_int_s = r'(.+)::.*International'
@@ -134,3 +269,5 @@ class SeriesProcessor(object):
             g = Genre.add(genre)
             m.genres.add(g)
         return m
+
+#TODO: Class to add Episode
